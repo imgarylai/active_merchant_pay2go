ef9e9b190d51728b5e643739aaf7fbe1b1661eea
chenjenping <chen.jen.ping@hotmail.com>
2018-07-22 23:55:19 +0800

    update api version to 1.4

--- lib/offsite_payments/integrations/pay2go/notification.rb
+++ lib/offsite_payments/integrations/pay2go/notification.rb
@@ -8,7 +8,7 @@ module OffsitePayments #:nodoc:
         PARAMS_FIELDS = %w(
           Status Message MerchantID Amt TradeNo MerchantOrderNo PaymentType RespondType CheckCode PayTime IP
           EscrowBank TokenUseStatus RedAmt RespondCode Auth Card6No Card4No Inst InstFirst InstEach ECI PayBankCode
-          PayerAccount5Code CodeNo BankCode Barcode_1 Barcode_2 Barcode_3 ExpireDate ExpireTime CheckCode
+          PayerAccount5Code CodeNo BankCode Barcode_1 Barcode_2 Barcode_3 ExpireDate ExpireTime Version TradeInfo TradeSha
         )
 
         PARAMS_FIELDS.each do |field|
@@ -33,12 +33,20 @@ module OffsitePayments #:nodoc:
         end
 
         def checksum_ok?
-          raw_data = URI.encode_www_form OffsitePayments::Integrations::Pay2go::CHECK_CODE_FIELDS.sort.map { |field|
+          key = OffsitePayments::Integrations::Pay2go.hash_key
+          iv = OffsitePayments::Integrations::Pay2go.hash_iv
+
+          aes = OpenSSL::Cipher.new('AES-256-CBC')
+          aes.encrypt
+          aes.key = key
+          aes.iv = iv
+
+          raw_data = URI.encode_www_form OffsitePayments::Integrations::Pay2go::CHECK_CODE_FIELDS.map { |field|
             [field, @params[field]]
           }
 
-          hash_raw_data = "HashIV=#{OffsitePayments::Integrations::Pay2go.hash_iv}&#{raw_data}&HashKey=#{OffsitePayments::Integrations::Pay2go.hash_key}"
-          Digest::SHA256.hexdigest(hash_raw_data).upcase == check_code
+          trade_info = (aes.update(raw_data) + aes.final).unpack('H*').first
+          Digest::SHA256.hexdigest("HashKey=#{key}&#{trade_info}&HashIV=#{iv}").upcase == trade_sha
         end
       end
     end
